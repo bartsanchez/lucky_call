f153f2cdaebde104332acd6aef59b2f5cff64781
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-08-10 20:35:51 +0200

    Add Guess model

--- lucky_call/lucky_call/settings.py
+++ lucky_call/lucky_call/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'guess.apps.GuessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
