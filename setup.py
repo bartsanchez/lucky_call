15517948217dbb947074d87eb2b225e7bfcbbe16
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-08-10 21:19:06 +0200

    Add tox to run tests

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='lucky_call',
+    description='Lucky Call project',
+
+    version='0.1',
+
+    author='Bartolome Sanchez',
+    author_email='bsanchezsalado@gmail.com',
+
+    packages=(setuptools.find_packages()),
+)
