60517407c3e66a0edd88c10068ab7202d6979bc6
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-08-12 22:12:25 +0200

    Remove secret_key envvar

--- lucky_call/lucky_call/settings/base.py
+++ lucky_call/lucky_call/settings/base.py
@@ -3,7 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'fake_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
