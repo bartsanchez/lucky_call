f3eda164ef85f6df8fac5d898946f3c72ee76523
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-08-12 22:07:37 +0200

    Move settings to base settings

--- lucky_call/manage.py
+++ lucky_call/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lucky_call.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lucky_call.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
