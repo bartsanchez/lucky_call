1a3c47ed9bf4a26fe0e49172119044cba58aa4c8
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2019-08-16 16:10:34 +0200

    Add script to generate sample requests

---
+++ scripts/generate_sample_requests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import argparse
+import json
+import random
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Generate sample requests')
+    parser.add_argument('number_of_samples', type=int)
+    parser.add_argument('keyword', type=str)
+
+    return parser.parse_args()
+
+
+def main(args):
+    curl_cmd = (
+        'curl '
+        '--header "Content-Type: application/json" '
+        'http://localhost/ -d'
+    )
+    for i in range(args.number_of_samples):
+        user_id = random.randint(0, 999999)
+        user_email = 'user{}@example.com'.format(user_id)
+        if i % 13 == 0:
+            keyword = 'wrong_keyword'
+        else:
+            keyword = args.keyword
+        # 5 users of each 1000 will use a number of two digits
+        # in order to generate wrong requests
+        number = random.randint(95, 999)
+        data = {
+            'user_email': user_email,
+            'keyword': keyword,
+            'number': number,
+        }
+        line = json.dumps(data).replace('"', r'\"')
+        print('{} "{}" &'.format(curl_cmd, line))
+
+
+if __name__ == '__main__':
+    main(get_args())
